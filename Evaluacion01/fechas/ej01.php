3a68d5c445e5f675f63639c22eb4fe01353963bc
Hector Santos <hecsanroq@gmail.com>
2018-11-05 16:53:28 +0100

    zxvbzdfbdfb

--- Evaluacion01/fechas/ej01.php
+++ Evaluacion01/fechas/ej01.php
@@ -3,15 +3,35 @@
 1. Realiza una función que acepte una fecha como cadena con el formato dd-mm-aaaa
 compruebe si la fecha es correcta y nos devuelva la fecha en formato UNIX.
  */
+
+/*
+ function func01($date, $format = 'd-m-Y')
+{
+    $d = DateTime::createFromFormat($format, $date);
+   
+    return $d && $d->format($format) === $date;
+}
+if(func01('21-11-2018')==true){
+    echo "correcto";
+}else{
+    echo "Formato (dd-mm-YYYY)";
+}
+ */
+
+
+
  function func01($fecha) {
-    $day=1<=>31;
-    $month=1<=>12;
-    $year=1<=>9999;
-     $fecha=checkdate($day, $month, $year);
-     if($fecha==true){
-        echo "Correcta";
-     }echo "El formato correcto es dd-mm-aaaa";
+     $fecha2=$fecha;
+     $fecha_con_formato = date('d-m-Y',$fecha2);
+     return $fecha_con_formato;
+     
  }
  
- func01(10/11/18);
+ if(func01('21-11-2018')==true){
+     echo "correcto";
+ }else{
+     echo "Formato (dd-mm-YYYY)";
+ }
+
+ 
  

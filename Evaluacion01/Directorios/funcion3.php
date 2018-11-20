a5e91aa2de2efda656d3cac9d58394f6bdaafef8
Hector Santos <hecsanroq@gmail.com>
2018-11-20 21:27:38 +0100

    bueno joder!!!!!!!

---
+++ Evaluacion01/Directorios/funcion3.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ Función que recorre directorio y subdirectorios.
+ Realiza una función devuelveDirSubdir que recorre
+ un directorio y sus subdirectorios devolviendo los
+ nombre de los ficheros que contienen cada uno de
+ ellos, discriminando donde está contenido cada uno
+ de ellos. Devuelve array multidimensional.
+ */
